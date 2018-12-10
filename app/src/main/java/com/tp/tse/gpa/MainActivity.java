6008784f78d56c00a5fb74177cf15a8c43687b56
QC858020 <qiuchen858020@hotmail.com>
2018-12-10 08:55:47 +0100

    First Interface

--- app/src/main/java/com/tp/tse/gpa/MainActivity.java
+++ app/src/main/java/com/tp/tse/gpa/MainActivity.java
@@ -2,12 +2,19 @@ package com.tp.tse.gpa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textViewName;
+    private Button buttonNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewName  = findViewById(R.id.activity_main_textview_name);
+        buttonNext = findViewById(R.id.activity_main_button_next);
     }
 }
