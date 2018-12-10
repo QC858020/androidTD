d3401085387b640e7245042315d8d411b5cf623b
QC858020 <qiuchen858020@hotmail.com>
2018-12-10 10:19:06 +0100

    Second interface

---
+++ app/src/main/java/com/tp/tse/gpa/FormActivity.java
@@ -0,0 +1,37 @@
+package com.tp.tse.gpa;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class FormActivity extends AppCompatActivity {
+    private Button buttonSave;
+    private EditText editTextName;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+
+        buttonSave = findViewById(R.id.activity_form_button);
+        editTextName = findViewById(R.id.activity_form_edittext_name);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save();
+            }
+        });
+    }
+
+    private void save() {
+        String name = editTextName.getText().toString();
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(name: "USERNAME", name);
+        setResult(RESULT_OK,resultIntent);
+        finish();
+    }
+}
--- app/src/main/java/com/tp/tse/gpa/MainActivity.java
+++ app/src/main/java/com/tp/tse/gpa/MainActivity.java
@@ -1,11 +1,16 @@
 package com.tp.tse.gpa;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private TextView textViewName;
     private Button buttonNext;
 
@@ -15,6 +20,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textViewName  = findViewById(R.id.activity_main_textview_name);
+
         buttonNext = findViewById(R.id.activity_main_button_next);
+
+        buttonNext.setOnClickListener(this);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK && data.getExtras() != null) {
+                Log.d( "", "");
+            } else {
+                ;
+            }
+        }
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.activity_main_button_next:
+                Intent intent = new Intent(this,FormActivity.class);
+
+                startActivityForResult(intent, 1);
+                break;
+        }
     }
 }
